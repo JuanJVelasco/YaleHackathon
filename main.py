36a471e8afbcc95d68d35d869b9b033e2414db6d
jc <johncain1018@gmail.com>
2023-02-25 12:14:17 -0500

    setting up flask

--- main.py
+++ main.py
@@ -1,1 +1,27 @@
-print("hello, world")
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    # return render_template('pages/placeholder.home.html')
+    return "hello, world"
+
+@app.route('/signup')
+def signup():
+    # return render_template('pages/placeholder.home.html')
+    return "signup"
+
+@app.route('/login')
+def login():
+    # return render_template('pages/placeholder.home.html')
+    return "hello, world"
+
+@app.route('/dashboard')
+def dashboard():
+    # return render_template('pages/placeholder.home.html')
+    return "hello, world"
+
+
+if __name__ == '__main__':
+    app.run()
