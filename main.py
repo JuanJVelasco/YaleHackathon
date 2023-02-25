a37b4040a0e15a71e010f2d87efe86c9cca035f5
jc <johncain1018@gmail.com>
2023-02-25 16:00:06 -0500

    video stream over flask

--- main.py
+++ main.py
@@ -1,27 +1,28 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, Response
+import cv2
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='static')
+
+video = cv2.VideoCapture(0)
 
 @app.route('/')
-def home():
-    # return render_template('pages/placeholder.home.html')
-    return "hello, world"
+def index():
+    return render_template('main.html', video_feed=gen(video))
 
-@app.route('/signup')
-def signup():
-    # return render_template('pages/placeholder.home.html')
-    return "signup"
+def gen(video):
+    while True:
+        success, image = video.read()
+        image = cv2.flip(image, 1)
+        ret, jpeg = cv2.imencode('.jpg', image)
+        frame = jpeg.tobytes()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
 
-@app.route('/login')
-def login():
-    # return render_template('pages/placeholder.home.html')
-    return "hello, world"
-
-@app.route('/dashboard')
-def dashboard():
-    # return render_template('pages/placeholder.home.html')
-    return "hello, world"
-
+@app.route('/video_feed')
+def video_feed():
+    global video
+    return Response(gen(video),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0', port=2204, threaded=True, debug=True)
